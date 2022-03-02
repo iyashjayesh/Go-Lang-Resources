1bd183ca9c969f8c029d76ba0a4018b9b92ce161
iyashjayesh <er.yashjayeshchauhan@gmail.com>
2022-03-02 13:04:29 +0530

    made some changes in mod file

--- learn_with_code_snippets/03_packages/main.go
+++ learn_with_code_snippets/03_packages/main.go
@@ -1,12 +1,13 @@
 package main
 
-import(
+import (
 	"fmt"
 	"math" // this is the package
-	"github.com/iyashjayesh/go-lang-cheatsheet/03_packages/strutil" // this is the package
+
+	"github.com/iyashjayesh/go-lang-resources/learn_with_code_snippets/03_packages/strutil" // this is the package
 )
 
-func main(){
+func main() {
 	fmt.Println(math.Floor(22.12))
 	fmt.Println(math.Ceil(22.12))
 	fmt.Println(math.Sqrt(9))
