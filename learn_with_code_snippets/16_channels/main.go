93055b93fc4a97a14acb9cdc43921a870ddac5f4
Yash <180303105046@paruluniversity.ac.in>
2022-03-03 15:15:28 +0530

    GO Channels Added

--- learn_with_code_snippets/16_channels/main.go
+++ learn_with_code_snippets/16_channels/main.go
@@ -5,7 +5,26 @@ import "fmt"
 //Go Channels
 //Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine.
 //Go channels are typed by the values they convey.
+// Channels is used to share data between gorountines.
+// <-
+
+func foo(c chan int, someValue int) {
+	c <- someValue * 5
+}
 
 func main() {
 	fmt.Println("Go Channels")
+
+	//making a channel
+	fooVal := make(chan int)
+
+	go foo(fooVal, 5)
+	go foo(fooVal, 10)
+
+	// v1 := <- fooVal
+	// v2 := <- fooVal
+
+	v1, v2 := <-fooVal, <-fooVal
+	fmt.Println(v1, v2)
+
 }
