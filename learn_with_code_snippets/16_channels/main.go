907d576cc563c8694f5ae94d7d00498ab2cdb9a3
iyashjayesh <er.yashjayeshchauhan@gmail.com>
2022-03-03 14:43:35 +0530

    Go Channels Added

---
+++ learn_with_code_snippets/16_channels/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+//Go Channels
+//Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine.
+//Go channels are typed by the values they convey.
+
+func main() {
+	fmt.Println("Go Channels")
+}
